f76110129346635e6d3c26c51633fbce617455eb
Xingo <371991717@qq.com>
2016-10-29 16:05:32 +0800

    ch2: helloworld ch3: function

--- ch2/helloworld.go
+++ ch2/helloworld.go
@@ -118,6 +118,11 @@ func main() {
 		panic("panic here")
 	}))
 
+	var arr [10] int
+	arr[0] = 42
+	arr[1] = 13
+	fmt.Printf("The first element is %d\n", arr[0])
+
 }
 
 func throwsPanic(f func()) (b bool) {
